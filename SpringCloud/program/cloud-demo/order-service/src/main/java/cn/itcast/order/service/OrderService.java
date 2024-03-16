2c9434270a7dddf3b7f8a59011798be1cc5f9667
只会CV <528375822@qq.com>
2024-03-16 10:34:01 +0800

    SpringCloud练习项目同步

---
+++ SpringCloud/program/cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,34 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2.利用RestTemplate发起http请求，查询用户
+        // 参数1：URL路径，这个和浏览器里的路径是完全一样的
+        // 2.1 url路径
+        String url = "http://userservice/user/" + order.getUserId();
+        // 2.2 发送http请求：实现远程调用
+        // 得到的结果是一个JSON风格，但我们想要的是user对象，但是RestTemplate非常的智能
+        // 参数2：responseType 返回值类型。例如这里填User.class，它会自动帮你反序列化成User类型
+        User user = restTemplate.getForObject(url, User.class);
+        // 封装user到Order
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+}
---
+++ SpringCloud/program/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -0,0 +1,32 @@
+package cn.itcast.user.web;
+
+import cn.itcast.user.pojo.User;
+import cn.itcast.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 路径： /user/110
+     *
+     * @param id 用户id
+     * @return 用户
+     */
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id) {
+        return userService.queryById(id);
+    }
+}
