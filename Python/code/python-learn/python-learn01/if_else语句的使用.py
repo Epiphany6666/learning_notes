08e3f8b610319ff282c994b9f4246a2e192a56d5
只会CV <528375822@qq.com>
2024-03-25 13:23:14 +0800

    午睡前python学习

--- Python/code/python-learn/python-learn01/if_else语句的使用.py
+++ Python/code/python-learn/python-learn01/if_else语句的使用.py
@@ -1,6 +1,26 @@
-# 这里注意input得到的永远是字符串类型的数据，这里需要强转为int
-age = int(input("请输入你的年龄"))
-if age >= 18:
-    print("您已成年，游玩需补票10元！")
-else:
-    print("您未成年，可以免费游玩！")
+# # 这里注意input得到的永远是字符串类型的数据，这里需要强转为int
+# age = int(input("请输入你的年龄"))
+# if age >= 18:
+#     print("您已成年，游玩需补票10元！")
+# else:
+#     print("您未成年，可以免费游玩！")
+import random
+
+# if int(input("请输入你的身高")) < 120:
+#     print("")
+
+# num = 10
+# if int(input("请输入第一次猜想的数字")) == num:
+#     print("恭喜你猜对啦");
+# elif int(input("不对，再猜一次")) == num:
+#     print("恭喜你猜对啦");
+# elif int(input("不对，再猜最后一次")) == num:
+#     print("恭喜你猜对啦！")
+# else:
+#     print("Sorry, 全部猜错啦，我想的是：%d" % num)
+
+num = random.random(1, 10)
+if int(input("请猜第一次")) != 10:
+    if int(input())
+else
+    print("猜对啦")
