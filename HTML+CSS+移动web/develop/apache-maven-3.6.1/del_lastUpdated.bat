1697f3348fc3b036bd4b9e04f72f8f0d04167607
只会CV <528375822@qq.com>
2024-04-01 15:11:39 +0800

    delete

--- HTML+CSS+移动web/develop/apache-maven-3.6.1/del_lastUpdated.bat
+++
@@ -1,7 +0,0 @@
-set REPOSITORY_PATH=O:\BaiduSyncdisk\develope\apache-maven-3.6.1\maven-repo
-rem 正在搜索...
-
-del /s /q %REPOSITORY_PATH%\*.lastUpdated
-
-rem 搜索完毕
-pause
---
+++ Python/code/python-learn/python-learn01/自动查核酸.py
@@ -0,0 +1,9 @@
+# def auto():
+#     print("欢迎")
+#     print("请你")
+#
+# auto()
+
+# def add(x, y):
+#     return x + y;
+# print(add(3, 4))
