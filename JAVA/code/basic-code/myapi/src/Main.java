366147dc66d5ab51bced2fe39c7882539aa983fb
只会CV <528375822@qq.com>
2024-04-23 18:50:00 +0800

    一天Java的学习

--- JAVA/code/basic-code/myapi/src/Main.java
+++ JAVA/code/basic-code/myapi/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //需求1:打印时间原点开始一年之后的时间
+        //1.创建一个对象，表示时间原点
+        Date d1 = new Date(0L);
+
+        //2.获取d1时间的毫秒值
+        long time = d1.getTime();
+
+        //3.在这个基础上我们要加一年的毫秒值即可
+        time = time + 1000L * 60 * 60 * 24 * 365;
+        //4.把计算之后的时间毫秒值，再设置回d1当中
+        d1.setTime(time);
+        //5.打印d1
+        System.out.println(d1);
     }
 }
