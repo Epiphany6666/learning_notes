f7e16425a0058d2c08495857ecbd661ace5e3f4a
只会CV <528375822@qq.com>
2024-05-03 18:29:39 +0800

    下午Java的学习

---
+++ JAVA/code/basic-code/myio/src/test1.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class test1 {
+    /**
+     * 拷贝一个文件夹，考虑子文件夹
+     */
+    public static void main(String[] args) throws IOException {
+        File file = new File("");
+        copyFiles(file);
+    }
+
+    private static void copyFiles(File src) throws IOException {
+        File[] files = src.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    FileInputStream fis = new FileInputStream("");
+                    FileOutputStream fos = new FileOutputStream("拷贝的目的地");
+                    byte[] bytes = new byte[1024 * 1024 * 5];
+                    int len;
+                    while ((len = fis.read(bytes)) != -1) {
+                        fos.write(bytes, 0, len);
+                    }
+                } else {
+                    copyFiles(file);
+                }
+            }
+        }
+    }
+}
