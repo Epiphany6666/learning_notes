f3093a2d23b4a7c8b59b5cc31f4d00ef3be9bb34
只会CV <528375822@qq.com>
2024-05-25 18:30:00 +0800

    下午redis的学习

--- Redis/program/hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ Redis/program/hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,9 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.SystemConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +23,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result queryById(Long id) {
+        // 1.从redis查询商铺缓存，查询的时候需要选择一个数据结构，应该选哈希还是string呢？这里存的商铺肯定是一个对象，存储一个对象很多同学会说选哈希。
+        // 没错，选哈希完全可以，因为之前演示的时候都是用哈希来演示，这里就用string来演示。
+        // 店铺key的选择要确保唯一，现在传店铺，每一个店铺都要有一个唯一的key，店铺的id就是唯一的，因此在这里直接使用id作为key。当然我们需要一个前缀
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2.判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3.存在，直接返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 4.不存在，根据id查询数据库
+        Shop shop = getById(id);
+        // 5.查询数据库不存在，直接返回错误
+        if (shop == null) {
+            return Result.fail("店铺不存在！");
+        }
+        // 6.存在，写入redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        // 7.返回
+        return Result.ok(shop);
+    }
 }
